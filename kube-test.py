646a7b10a57bbb8b04939929661c46bac8627430
Andrei Plotoaga <andpl509@student.liu.se>
2023-05-19 14:22:20 +0200

    test config

--- kube-test.py
+++ kube-test.py
@@ -574,7 +574,7 @@ async def main():
                     actions[inp]()
 
             elif inp == "debug":
-                await start_case("LC 5 NLC 2")
+                await start_case("LC 1 NLC 1")
 
             elif inp == "wa":
                 # debug no-kube here
